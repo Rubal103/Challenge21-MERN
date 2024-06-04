39b24900e6864116535dc8c60ddc1b7f8cb3e4bb
Rubal <rubal103@gmail.com>
2024-06-04 13:28:15 -0400

    updated auth to work with GraphQl/applied Apolloserver to express

--- server/server.js
+++ server/server.js
@@ -1,21 +1,49 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
+
+//const routes = require('./routes');
+
+//using resovers and typeDefs now
+const { typeDefs, resolvers } = require('./schemas');
+
+//added Apolloserver and applied it to express server as middleware
+const { ApolloServer } = require('@apollo/server');
+const { expressMiddleware } = require('@apollo/server/express4');
+const { authMiddleware } = require('./utils/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+//created a new instance of ApolloServer with schemas (graphql)
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+//waiting for the server (new instance created above) to start 
+const startApolloServer = async () => {
+await server.start();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+//using graphql
+app.use('/graphql', expressMiddleware(server, {
+  context: authMiddleware
+}));
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+//app.use(routes);--no longer using routes
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  console.log(`API server running on port ${PORT}!`);
+  console.log(`GraphQL at http://localhost:${PORT}/graphql`);
 });
+};
+
+startApolloServer();
--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,3 +1,5 @@
+//needed dotenv
+require("dotenv").config();
 const jwt = require('jsonwebtoken');
 
 // set token secret and expiration date
@@ -6,7 +8,9 @@ const expiration = '2h';
 
 module.exports = {
   // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
+
+  //using middleware
+  authMiddleware: function ({req}) {
     // allows token to be sent via  req.query or headers
     let token = req.query.token || req.headers.authorization;
 
@@ -16,7 +20,9 @@ module.exports = {
     }
 
     if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
+      //return res.status(400).json({ message: 'You have no token!' });
+
+      return req;//no longer returning res
     }
 
     // verify token and get user data out of it
@@ -25,11 +31,12 @@ module.exports = {
       req.user = data;
     } catch {
       console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
+      //return res.status(400).json({ message: 'invalid token!' });--no longer returning res
     }
 
-    // send to next endpoint
-    next();
+    return req;
+
+    //next();---returing req and not sengin to next endpoint
   },
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
